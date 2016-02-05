b362ed7678abadf14a56d64de2fe43f264e6d048
Agustin Ramos <evilnapsis@gmail.com>
2016-02-05 10:54:12 -0600

    set display errors on

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+$debug= true;
+if($debug){
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+}
+
 include "core/autoload.php";
 ob_start();
 session_start();
