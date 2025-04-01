e617d9a09fb6ff866f29060b41ebcd771daddb7c
unknown <evilnapsis@gmail.com>
2025-03-31 23:04:49 -0700

    update database to php 8

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -2,11 +2,16 @@
 class Database {
 	public static $db;
 	public static $con;
+	public $user;
+	public $pass;
+	public $host;
+	public $ddbb;
 	function Database(){
 		$this->user="root";$this->pass="";$this->host="localhost";$this->ddbb="inventiolite";
 	}
 
 	function connect(){
+		$this->user="root";$this->pass="";$this->host="localhost";$this->ddbb="inventiolite";
 		$con = new mysqli($this->host,$this->user,$this->pass,$this->ddbb);
 		$con->query("set sql_mode=''");
 		return $con;
